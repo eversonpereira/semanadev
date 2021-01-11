f3ce1d303ec4649d8dd59fe6f6a6f857973eb25c
Everson Pereira <everson@everson.net.br>
2021-01-11 11:06:40 -0300

    first commit

---
+++ src/main/java/br/gov/sp/franca/sds/SdsApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.franca.sds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsApplication.class, args);
+	}
+
+}
